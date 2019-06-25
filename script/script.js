553511a57dde60b3840f927ad1b390def7c92c5a
makccr <mackenziegcriswell@gmail.com>
2019-06-25 13:26:09 -0500

    Initial Commit

---
+++ script/script.js
@@ -0,0 +1,6 @@
+function mobileHam() {
+  document.getElementById("mobileHam").style.display = "block";
+}
+function mobileHamX() {
+  document.getElementById("mobileHam").style.display = "none";
+}
