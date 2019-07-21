f3b6f399fac73b07b7c3a62772e18801ac5cec79
Mack <mackenziegcriswell@gmail.com>
2019-07-20 19:10:32 -0500

    Updating blog formatting

--- script/script.js
+++ script/script.js
@@ -1,6 +1,13 @@
 function mobileHam() {
   document.getElementById("mobileHam").style.display = "block";
 }
+
 function mobileHamX() {
   document.getElementById("mobileHam").style.display = "none";
+}
+
+function copyText() {
+  var copyText = document.getElementById("blogLink");
+  copyText.select();
+  document.execCommand("copy");
 }
