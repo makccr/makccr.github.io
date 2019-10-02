38214dc24cd6e218e05ff38a5d1f8a4a42d9df80
Mack <mackenziegcriswell@gmail.com>
2019-10-01 23:08:08 -0500

    Updating css/javascript

---
+++ script/main.js
@@ -0,0 +1,8 @@
+function mobileMenu() {
+    var x = document.getElementById("mobile-menu")
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
