c1631f2b77c5a817b4b5e32e6fe2b2d09bea471a
Mackenzie Criswell <mackenziegcriswell@gmail.com>
2025-02-19 02:22:06 -0600

    Removing the rest of the old site

--- script/main.js
+++
@@ -1,8 +0,0 @@
-function mobileMenu() {
-    var x = document.getElementById("mobile-menu")
-    if (x.style.display === "none") {
-        x.style.display = "block";
-    } else {
-        x.style.display = "none";
-    }
-}
--- script/menu.js
+++
@@ -1,8 +0,0 @@
-function mobileMenu() {
-    var x = document.getElementById("mobile-links")
-    if (x.style.display === "none") {
-        x.style.display = "block";
-    } else {
-        x.style.display = "none";
-    }
-}
