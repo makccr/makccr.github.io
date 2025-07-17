87410dc86fbc9bcca3d4e3210d72bd8b99a2380a
Mackenzie Criswell <mackenziegcriswell@gmail.com>
2025-07-17 17:34:42 -0500

    attempting to adress unexpected behavior with downloadable assets

--- eleventy.config.js
+++ eleventy.config.js
@@ -5,8 +5,10 @@ export default function (eleventyConfig) {
 	eleventyConfig.addPlugin(eleventyImageTransformPlugin);
     eleventyConfig.addPassthroughCopy("bundle.css");
     eleventyConfig.addFilter("postDate", dateObj => {
+	eleventyConfig.addPassthroughCopy("downloads");
   return DateTime.fromJSDate(dateObj).toFormat("LLLL d, yyyy")
 })
+
 };
 
 
